4da84cb8a1afd02ebdfb783db09f0f1f3c0e2ead
fransafu <fransafu@gmail.com>
2017-08-15 22:10:46 -0300

    Reboot y requirements

--- Client/main.py
+++ Client/main.py
@@ -1,23 +1,27 @@
 import pycurl
 from   io import BytesIO
 from   flask import Flask
+import subprocess
 
 app = Flask(__name__)
 
 def getIP():
-	buffer = BytesIO()
-	c = pycurl.Curl()
-	c.setopt(c.URL, 'ipinfo.io/ip')
-	c.setopt(c.WRITEDATA, buffer)
-	c.perform()
-	c.close()
-	body = buffer.getvalue()
-	return body.decode('iso-8859-1').rstrip()
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'ipinfo.io/ip')
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+    body = buffer.getvalue()
+    return body.decode('iso-8859-1').rstrip()
+
+def reboot():
+    result = subprocess.run(['reboot', '-f'], stdout=subprocess.PIPE)
 
 def main():
-	print ("Iniciando cliente")
-	IP = getIP()
-	print (IP)
+    print ("Iniciando cliente")
+    IP = getIP()
+    print (IP)
 
 if __name__ == '__main__':
-	main()
+    main()
